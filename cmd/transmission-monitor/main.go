ee84e9d176df7a7a769f01bebeb2e30dbc67c489
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2022-06-05 00:11:39 +0300

    WIP

--- cmd/transmission-monitor/main.go
+++ cmd/transmission-monitor/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/spf13/viper"
+	"github.com/hekmon/transmissionrpc/v2"
 )
 
 var version = "undefined"
@@ -27,11 +29,43 @@ func run() int {
 	viper.SetConfigType("yaml")
 	viper.SetConfigFile(*configFilename)
 	setDefaults(viper.GetViper())
-
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("unable to read config file: %s", err)
 	}
 
+	trpc, err := transmissionrpc.New(
+		viper.GetString("rpc.host"),
+		viper.GetString("rpc.user"),
+		viper.GetString("rpc.password"),
+		&transmissionrpc.AdvancedConfig{
+			HTTPS: viper.GetBool("rpc.https"),
+			Port: uint16(viper.GetUint32("rpc.port")),
+			RPCURI: viper.GetString("rpc.uri"),
+			HTTPTimeout: viper.GetDuration("rpc.httptimeout"),
+			UserAgent: viper.GetString("rpc.useragent"),
+			Debug: viper.GetBool("rpc.debug"),
+		},
+	)
+	if err != nil {
+		log.Fatalf("unable to construct transmission RPC client: %v", err)
+	}
+
+	torrents, err := trpc.TorrentGetAll(context.Background())
+	if err != nil {
+		log.Fatalf("unable to get torrents: %v", err)
+	}
+
+	seenError := false
+	for _, torrent := range torrents {
+		if torrent.ErrorString != nil && *torrent.ErrorString != "" {
+			fmt.Printf("torrent hash=%q, error=%q", *torrent.HashString, *torrent.ErrorString)
+			seenError = true
+		}
+	}
+
+	if seenError {
+		return 3
+	}
 
 	return 0
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.18
 
 require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hekmon/cunits/v2 v2.1.0 // indirect
+	github.com/hekmon/transmissionrpc/v2 v2.0.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
